e151246088a9a243034ed1c8d7f9eb9b2ef20d91
Brent-the-carpenter <Beh337@gmail.com>
2023-11-25 00:20:43 -0500

    added css and reworked java script

--- game.js
+++ game.js
@@ -2,36 +2,37 @@
 
 
 
-    function getComputerChoice()  {
-        const Choices = ["rock", "paper", "scissors"];
-        return Choices[Math.floor(Math.random() * 3)];
-        
-    }
   
    
+let round = document.querySelector('div.results');
+round.classList.add('gametext'); 
 
+let gameWinner = document.createElement("div");
+gameWinner.classList.add('gametext');
 
    let playerScore = 0 ; // keeps track of player score
    let computerScore = 0 ; // keeps track of computer score
     
-    for ( let round = 0 ; round < 5 ; round ++){ // loop that runs the game 5 times through
-     
-        console.log(game());
+   let playerSelction = " ";
+        const buttons = document.querySelectorAll('button');
         
-    }
+        buttons.forEach((button) =>{
+             button.addEventListener('click', () => {
+             playerSelction = button.id;
+                game()
+              });
+         });
     
-    
-    
-    
+        
     function game(){
         function getComputerChoice()  { // gets computers random choice using an array and picks one string using math random
             const Choices = ["rock", "paper", "scissors"];
             return Choices[Math.floor(Math.random() * 3)];
-            
-        }
+            }
+
         const computerSelection = getComputerChoice(); // stores the computers random choice in variable used for parameter
-        playerSelction = prompt("Please make your choice of rock , paper or scissors!") ; // takes users input and stores it in player section
-        playerScore == 0; // sets score to zero for each round
+        
+         playerScore == 0; // sets score to zero for each round
         computerScore == 0; // sets score to zero for each round 
         
         function rockPaperScissor(playerSelction , computerSelection){
@@ -42,40 +43,56 @@
                     if (playerSelction === "rock" && computerSelection === "scissors" || 
                     playerSelction === "paper" && computerSelection === "rock" || 
                     playerSelction === "scissors" && computerSelection === "paper" ) {
-                        alert("You win!!");
-                        playerScore++ ;
-                        return (`You win! ${playerSelction} beats ${computerSelection} 
-                        your score is ${playerScore} the computer score is ${computerScore}` ) ;
+                        ++playerScore;
+                        round = document.querySelector("div.results");
+                        round.textContent=
+                         (`You win! ${playerSelction} beats ${computerSelection} 
+                         The computer score is ${computerScore}  Your score is ${playerScore} ` ) ;
+                        
                     }
                     else if (playerSelction === computerSelection){
-                        alert("It is a Draw!!")
-                        return(`Draw! ${playerSelction} and ${computerSelection} are equal.
-                        your score ${playerScore}  the computer score ${computerScore}`);
+                        round = document.querySelector("div.results");
+                        round.textContent = 
+                        (`Draw! ${playerSelction} and ${computerSelection} are equal.
+                        The computer score ${computerScore}  Your score ${playerScore}  `);
+                        
                     }
                     else {
-                        alert("You lose!! Try again")
                         computerScore++;
-                        return( `You Lose! ${computerSelection} beats ${playerSelction} Try Again.
-                        the computer score is ${computerScore} your score is ${playerScore}`)
+                        round = document.querySelector("div.results");
+                        round.textContent =
+                        
+                        ( `You Lose! ${computerSelection} beats ${playerSelction} Try Again.
+                        The computer score is ${computerScore}     Your score is ${playerScore}`)
+                       
                     }   
                 
                 }
                
-       
-   
-                console.log(rockPaperScissor(playerSelction.toLowerCase(), computerSelection));
+       console.log(rockPaperScissor(playerSelction.toLowerCase(), computerSelection));
+
+           
+                if ((playerScore === 5) || (computerScore === 5)){
+                    if (playerScore > computerScore){
+                        round.appendChild(gameWinner);
+                        gameWinner.textContent = 'You won The Game!!!';
+                    } else if (playerScore < computerScore){
+                        round.appendChild(gameWinner);
+                        gameWinner.textContent = ' You Lost the Game. Please try again.';
+                    } else{
+                        round.appendChild(gameWinner);
+                        round.textContent = ' It Is a Draw. Both players tied!!!';
+                    }
+                }else{}
+        }
     
-        
-    }
-    //evaluates score to determine overall winner.
-            if (playerScore > computerScore){
-                alert("You win the game!!")
-            }else if (computerScore === playerScore){
-                alert("It is a Draw!!")
+    
+    
             
-            }else{ alert("You lose the game!! Try again")
+            
+           
         
                 
-            }
+            
 
                 
